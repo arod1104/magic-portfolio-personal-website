c63bd32735224b036ad7840917e1c1b72d66fc30
arod1104 <rodriguez.alex1828@gmail.com>
2025-05-18 16:17:27 -0500

    Disabled newsletter and added comment on where to add projects

--- src/app/resources/content.js
+++ src/app/resources/content.js
@@ -14,7 +14,7 @@ const person = {
 };
 
 const newsletter = {
-  display: true,
+  display: false,
   title: <>Subscribe to {person.firstName}'s Newsletter</>,
   description: (
     <>
@@ -253,6 +253,7 @@ const work = {
   title: `Projects – ${person.name}`,
   description: `Design and dev projects by ${person.name}`,
   // Create new project pages by adding a new .mdx file to app/blog/posts
+  // Create new project pages by adding a new .mdx file to app/blog/projects
   // All projects will be listed on the /home and /work routes
 };
 
